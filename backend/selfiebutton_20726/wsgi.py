5b327f3db7e2b9c1a9f83a5c42f42d9160b8bb4e
Crowdbotics <team@crowdbotics.com>
2020-09-26 13:38:54 +0000

    Update models

--- backend/selfiebutton_20726/wsgi.py
+++ backend/selfiebutton_20726/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'selfiebutton_20726.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "selfiebutton_20726.settings")
 
 application = get_wsgi_application()
